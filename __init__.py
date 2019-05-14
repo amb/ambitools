ed3a7a33bf49c0a1f865dd6c32dd169f524ea948
Tommi Hyppänen <thyppane@gmail.com>
2019-05-14 03:22:17 +0300

    Library as a submodule

--- __init__.py
+++ __init__.py
@@ -1,18 +1,17 @@
-﻿# ##### BEGIN GPL LICENSE BLOCK #####
-#
-#   This program is free software: you can redistribute it and/or modify
-#   it under the terms of the GNU General Public License as published by
-#   the Free Software Foundation, version 3 of the license.
+﻿"""
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-#   This program is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#   GNU General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-#   You should have received a copy of the GNU General Public License
-#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-# ##### END GPL LICENSE BLOCK #####
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
 
 
 import bpy
@@ -23,9 +22,9 @@ import mathutils as mu
 
 # import/reload all source files
 if "afm" not in locals():
-    from .amb import utils as au
-    from .amb import fastmesh as afm
-    from .amb import bbmesh as abm
+    from .bpy_amb import utils as au
+    from .bpy_amb import fastmesh as afm
+    from .bpy_amb import bbmesh as abm
 else:
     import importlib
 
